b82c943f20298642f1b1c54b2a573fde8fcda861
Bahaulddin Ahmad Shammary <bahaulddin.shammary@mycit.ie>
2019-10-22 21:06:07 +0100

    Python related code will be in this repository

--- Python/insert.py
+++
@@ -1,6 +0,0 @@
-from firebase import firebase
-
-firebase = firebase.FirebaseApplication('https://medi-screen.firebaseio.com/', None)
-data = {'Name': 'David Manilla', 'Email': 'dave.m@gmail.com', 'Addres': '111 Green place, Limerick', 'Age': 25, 'Weight': 80}
-res = firebase.post('/medi-screen/Patients', data)
-print(res)
