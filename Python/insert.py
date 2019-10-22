60edbafd95ddf7684463a06770cef7d18cfda70f
Bahaulddin Ahmad Shammary <bahaulddin.shammary@mycit.ie>
2019-10-22 21:13:28 +0100

    Python code insert data into firebase database

--- Python/insert.py
+++ Python/insert.py
@@ -1,6 +1,9 @@
 from firebase import firebase
 
-firebase = firebase.FirebaseApplication('https://medi-screen.firebaseio.com/', None)
-data = {'Name': 'David Manilla', 'Email': 'dave.m@gmail.com', 'Addres': '111 Green place, Limerick', 'Age': 25, 'Weight': 80}
-res = firebase.post('/medi-screen/Patients', data)
+firebase = firebase.FirebaseApplication('https://medi-screen.firebaseio.com/', None) # establish connection to database
+
+data = {'Name': 'David Manilla', 'Email': 'dave.m@gmail.com', 'Addres': '111 Green place, Limerick', 'Age': 25, 'Weight': 80} # create sample data
+
+res = firebase.post('/medi-screen/Patients', data) # use the connection to send data
+
 print(res)
