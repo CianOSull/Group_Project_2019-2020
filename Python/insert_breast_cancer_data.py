eb6f3792968f310cbec3d9d2de09664f052134cc
Bahaulddin Shammary <bahaulddin.shammary@mycit.ie>
2019-11-06 19:05:54 +0000

    Modified insert - Added few directories to .gitignore and created insert breast cancer data screipt

--- Python/insert_breast_cancer_data.py
+++ Python/insert_breast_cancer_data.py
@@ -1,8 +1,9 @@
 from firebase import firebase
+import numpy as np
 
 firebase = firebase.FirebaseApplication('https://medi-screen.firebaseio.com/', None) # establish connection to database
 
-data = {'Name': 'David Batista', 'Email': 'b.dave@gmail.com', 'Address': '134 Green place, Limerick', 'Age': 45, 'Weight': 120} # create sample data
+data = {'Name': 'Dave Batista', 'Email': 'b.dave@gmail.com', 'Address': '134 Green place, Limerick', 'Age': 45, 'Weight': 120} # create sample data
 
 res = firebase.post('/medi-screen/Patients', data) # use the connection to send data
 
